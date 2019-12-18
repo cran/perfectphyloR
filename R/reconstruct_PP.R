400cdf1a8d3b415850669544f41678adc18c7d5c
Charith Karunarathna <ckarunar@sfu.ca>
2019-12-18 05:40:03 +0000

    version 0.2.0

--- R/reconstruct_PP.R
+++ R/reconstruct_PP.R
@@ -18,7 +18,7 @@
 #'                  function.
 #' @param focalSNV  The column number of the focal SNV at which to reconstruct the reconstructed partitions.
 #' @param minWindow Minimum number of SNVs around the focal SNV in the window of SNVs used to reconstruct the
-#'                  partitions.
+#'                  partitions (default is the maximum of one and 2\% of the total number of the SNVs).
 #' @param sep       Character string separator to separate haplotype names for haplotypes
 #'                  that can not be distingushed in the window around the focal point. For example, if a tip is comprised
 #'                  of haplotypes "h1" and "h3", and sep = "-", then the tip label will be "h1-h3". The default 
@@ -58,16 +58,33 @@
 reconstructPP = function(hapMat, focalSNV, minWindow = 1, sep = "-") {
 
     
-    # Step 1: Select a window of SNVs about a focal SNV.
-    snvWin <- selectWindow(hapMat, focalSNV, minWindow)
-    #  The indices of the columns of the hapMat object that were used in the window.
-    snvWinIndices <- which(colnames(hapMat$hapmat) %in% colnames(snvWin$hapMat$hapmat)) 
-    #-----------------------------------------------------------------#
-    # Step 2: Build the tree for the window of SNVs from step 1.
-    dend <- buildDend(snvWin, sep = sep)
-    # Left bound and the right bound of the window of SNVs.
-    dend$snvWinIndices = snvWinIndices[c(1, length(snvWinIndices))]
+  nminWin = round(length(hapMat$posns)*.02, 0)  # 2% of nSNVs for default winsize.
+  
+  
+  # Step 1: Select a window of SNVs about a focal SNV.
+  if(minWindow == 1 & nminWin > 1){
     
-    return(dend)
+    snvWin <- selectWindow(hapMat, focalSNV, minWindow = nminWin)
+    
+  }else if(minWindow == 1 & nminWin == 1){
+    
+    snvWin <- selectWindow(hapMat, focalSNV, minWindow = 1)
+    
+  }else if(minWindow != 1 & nminWin > 1){   
+    snvWin <- selectWindow(hapMat, focalSNV, minWindow = minWindow)
+  }else{
+    snvWin <- selectWindow(hapMat, focalSNV, minWindow = 1)
+  }
+  
+  #  The indices of the columns of the hapMat object that were used in the window.
+  snvWinIndices <- which(colnames(hapMat$hapmat) %in% colnames(snvWin$hapMat$hapmat)) 
+  #-----------------------------------------------------------------#
+  # Step 2: Build the tree for the window of SNVs from step 1.
+  dend <- buildDend(snvWin, sep = sep)
+  # Left bound and the right bound of the window of SNVs.
+  dend$snvWinIndices = snvWinIndices[c(1, length(snvWinIndices))]
+  
+  return(dend)
+  
     
 }
